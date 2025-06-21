1294ce420b63c29b9aee339745a9828d282bb379
Isosa9 <sosayorleni72@gmail.com>
2025-06-20 19:00:57 -0600

    La funcion de buscar los productos en la base de datos

--- chatbot/db.py
+++ chatbot/db.py
@@ -6,7 +6,7 @@ from ia import consultar_ollama
 load_dotenv()
 DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_USER = os.getenv("DB_USER", "root")
-DB_PASSWORD = os.getenv("DB_PASSWORD","Sosaisis ")
+DB_PASSWORD = os.getenv("DB_PASSWORD","Sosaisis")
 DB_NAME = os.getenv("DB_NAME", "agrocomercial")
 
 def fetch_products(query: str, limit: int = 5):
